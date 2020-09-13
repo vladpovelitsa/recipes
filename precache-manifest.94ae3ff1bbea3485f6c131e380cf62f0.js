218499ca2e5b67808031793349ddd8b212493a07
vladpovelitsa <vlad.povelitsa@gmail.com>
2020-09-13 20:39:40 +0300

    Updates

---
+++ precache-manifest.94ae3ff1bbea3485f6c131e380cf62f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d5dbfaf8435bd21c196e9d927bff7f4",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "b26b9c2f11c9bebf06f9",
+    "url": "/recipes/static/css/main.fb0ac4ea.chunk.css"
+  },
+  {
+    "revision": "c61f629e989ee0853181",
+    "url": "/recipes/static/js/2.4d734789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipes/static/js/2.4d734789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26b9c2f11c9bebf06f9",
+    "url": "/recipes/static/js/main.89ef2d75.chunk.js"
+  },
+  {
+    "revision": "333e5e2704c8fddc52ed",
+    "url": "/recipes/static/js/runtime-main.da442e69.js"
+  }
+]);
